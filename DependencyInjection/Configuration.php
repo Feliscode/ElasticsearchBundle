07095b2fddff61845de57ae44c8c18b629a379b5
Dominik Pawelec <dominik.pawelec@g.pl>
2018-01-01 15:46:46 +0100

    Init commit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Feliscode\ElasticsearchBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * @author Dominik Pawelec <dominik@feliscode.com>
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('feliscode_elasticsearch');
+
+        return $treeBuilder;
+    }
+}
---
+++ FeliscodeElasticsearchBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Feliscode\ElasticsearchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Dominik Pawelec <dominik@feliscode.com>
+ */
+class FeliscodeElasticsearchBundle extends Bundle
+{
+}
